c63d56adcfe78dd7af5a7d5eb9c764ecc05b5909
iorate <682043+iorate@users.noreply.github.com>
2022-01-19 19:12:41 +0900

    chore: ignore /dist

--- dist/mpsl.d.ts
+++
@@ -1,9 +0,0 @@
-export declare type ParseResult = {
-    tld: string;
-    sld: string;
-    domain: string;
-    subdomain: string | null;
-};
-export declare function parse(domain: string): ParseResult | null;
-export declare function get(domain: string): string | null;
-export declare function isValid(domain: string): boolean;
